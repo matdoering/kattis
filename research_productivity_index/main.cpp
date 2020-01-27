a15b79656c0aeba59d1f099afaf8388fc59fb44c
Matthias <mdoering@mpi-inf.mpg.de>
2020-01-27 21:37:04 +0100

    research producitivity: runtime tests passed

--- research_productivity_index/main.cpp
+++ research_productivity_index/main.cpp
@@ -4,6 +4,9 @@
 #include <cassert>
 #include <algorithm>
 #include <iomanip>      // std::setprecision
+#include <bitset>
+
+static const int MAX_PAPERS = 100; // the maximum allowed nbr of papers accepted as an input
 
 void readData(int& nbrPapers, std::vector<double>& probs) {
     std::cin >> nbrPapers;
@@ -33,7 +36,7 @@ void printSet(std::vector<int>& subset) {
        std::cout << std::endl;
 }
 
-void generateSubsets(std::vector<std::vector<int>>& out, const std::vector<int>& data, int k, int pos, std::vector<int>& subset) {
+void generateSubsets(std::vector<std::bitset<MAX_PAPERS>>& out, const std::vector<int>& data, int k, int pos, std::bitset<MAX_PAPERS>& subset) {
    // out: all subsets of size k
    // data: construct subsets from these data
    // k: remaining elements that need to be added to subset
@@ -48,39 +51,40 @@ void generateSubsets(std::vector<std::vector<int>>& out, const std::vector<int>&
     }
     // recursion
     for (size_t i = pos; i < data.size(); ++i) {
-        subset.push_back(data[i]);
+        subset.set(data[i]);
         generateSubsets(out, data, k-1, i+1, subset);
-        subset.pop_back();
+        subset.reset(data[i]);
     }
 }
 
 
-// for n = 2: 
-// if we have k = 2 -> just return p1 * p2
-// for k = 1 -> p1 * (1-p2) + p2 * (1-p1)
-// for k = 0 -> 0
-//
-// for n = 3 and k = 2
-// 1st&2nd elem chosen= p1 * p2 * (1-p3) + 
-// 1st&3rd elem chosen= p1 * p3 * (1-p2) 
-// 2nd&3rd elem chosen= p2 * p3 * (1-p1)
+
+// calcaulates the probability to have k accepted papers
+// when n papers are submitted assuming that each papers
+// has an acceptance probability as provided in probs
+// // this calculates the Poisson binomial probability
+// https://en.wikipedia.org/wiki/Poisson_binomial_distribution
+// approximation with fourier transform would be possible
 double subsetProbability(const std::vector<double>& probs, int k, int n) {
     // k: nbr selected
     // n: nbr available
-    std::vector<std::vector<int>> combis; // all subsets of size k
-    std::vector<int> subset;
+    //
+    std::vector<std::bitset<MAX_PAPERS>> combis; // all subsets of size k
+    std::bitset<MAX_PAPERS> subset;
     std::vector<int> indices(n);
     for (int i =0; i < indices.size(); ++i) {
         indices[i] = i;
     }
-    generateSubsets(combis, indices, k, 0, subset); // TODO: performance -> pre-generate all subsets earlier
+    //std::cout << "generating subsets ... " << std::endl;
+    generateSubsets(combis, indices, k, 0, subset);
+    //std::cout << "subsets generated!" << std::endl;
     //std::cout << "nbr of subsets: " << combis.size() << std::endl;
     double P = 0.0; // probability of subset
     for (auto& subset: combis) {
         // prob of chosen and non chosen elements
         double p = 1.0;
         for (int i : indices) {
-            if (find(subset.begin(), subset.end(), i) != subset.end()) {
+            if (subset[i]) {
                 // paper included
                 p *= probs[i];
             } else {
@@ -115,6 +119,7 @@ double productivityIndex(double a, int s) {
     return std::pow(a, a/s);
 }
 
+/*
 double solveWithExpVal(const std::vector<double>& probs) {
     // incorrect
    std::vector<double> gains(probs.size(), 1);
@@ -149,6 +154,7 @@ double solveWithExpVal(const std::vector<double>& probs) {
     }
     return maxPrdIdx;
 }
+*/
 
 double solve(const std::vector<double>& probs) {
     // incorrect
@@ -161,19 +167,60 @@ double solve(const std::vector<double>& probs) {
    // generate all n choose k combinations of papers to be submitted
     int n = probs.size();
     double maxPrdIdx = 0.0;    
+
+
     for (size_t nSubmitted = 1; nSubmitted <= probs.size(); ++nSubmitted) {
         // generate all paper selections of size k
-        //std::cout << "##submission size: " << nSubmitted << std::endl;
+        std::cout << "n=" << nSubmitted << std::endl;
         double prdIdx = 0.0;
-        for (size_t nAccepted = 0; nAccepted <= nSubmitted; ++nAccepted) {
+        for (size_t nAccepted = 1; nAccepted <= nSubmitted; ++nAccepted) {
+            std::cout << "k=" << nAccepted << std::endl;
             // select publications to be considered as accepted
             double p = subsetProbability(probs, nAccepted, nSubmitted);
             //std::cout << "P: " << p << std::endl;
             double ePrdIdx = p*productivityIndex(nAccepted, nSubmitted);
             prdIdx += ePrdIdx;
-            //std::cout << "nbr accepted: " << nAccepted << "/" << nSubmitted << ", prdIdx: " << ePrdIdx << std::endl;
+            std::cout << "nbr accepted: " << nAccepted << "/" << nSubmitted << ", prdIdx: " << ePrdIdx << std::endl;
         }
-        //std::cout << "==> total prd idx: " << prdIdx << std::endl;
+        std::cout << "==> total prd idx: " << prdIdx << std::endl;
+        if (prdIdx > maxPrdIdx) {
+            maxPrdIdx = prdIdx;
+        }
+    }
+    return maxPrdIdx;
+}
+
+// we want an iterative algorithm to come up with the probabilities
+// p(k,n) for having k papers from n accepted
+// -> in every iteration we increase n and we cycle through all k's
+//
+double solveFast(const std::vector<double>& probs) {
+    // basis case: only 1 probability value provided as input
+    double maxPrdIdx = probs[0] * productivityIndex(1, 1);
+    std::vector<double> P = {probs[0], 1 - probs[0]}; // initial probabilities
+    // all other cases:
+    for (int n = 1; n < probs.size(); ++n) {
+        //std::cout << "n: " << n << std::endl;
+        double curProb = probs[n];
+        std::vector<double> curProbs = {P[0] * curProb}; // prob of choosing the 1st elem
+        for (int k = 0; k < n; ++k) {
+            // prob of choosing the  ... TODO
+            curProbs.push_back(P[k]*(1-curProb) + P[k+1] * curProb);
+        }
+        curProbs.push_back(P[n]*(1-curProb));
+        /*
+        std::cout << "cur probs:" << std::endl;
+        for (int i = 0; i < curProbs.size(); ++i) {
+            std::cout << curProbs[i] << ", ";
+        }
+        std::cout << std::endl;
+        */
+        P = curProbs; // update P
+        double prdIdx = 0.0;
+        for (int j =0; j < P.size(); ++j) { // j: nbr accepted papers
+            prdIdx += (P[j] * productivityIndex(n+1-j, n+1)); // submitted nbr of papers i n+1
+        }
+        // std::cout << "prd idx is: " << prdIdx << std::endl;
         if (prdIdx > maxPrdIdx) {
             maxPrdIdx = prdIdx;
         }
@@ -190,7 +237,8 @@ int main(int argc, char** argv) {
     std::sort(probs.begin(), probs.end(), std::greater<double>());
 
     std::vector<double> gains(probs.size(), 1);
-    double expectedPrdIdx = solve(probs);
+    //double expectedPrdIdx = solve(probs);
+    double expectedPrdIdx = solveFast(probs);
     std::cout << std::setprecision(20) << expectedPrdIdx << std::endl;
     /*
     std::cout << "TEST" << std::endl;
